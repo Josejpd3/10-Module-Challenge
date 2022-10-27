5c89a9b79cf6a414c1c9285a129f20c8e89b8313
Jose Plasencia <josejpd3@gmail.com>
2022-10-27 17:44:13 -0400

    tested return values with GETTERS

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -31,3 +31,35 @@ test('2 (S) should set name via contructor arguments', () => {
     const e = new Manager("name", "id", "email", testValue);
     expect(e.officeNumber).toBe(testValue);
   })
+
+  // GETTERS
+
+test('6 (G) should get name via getName()', () => { 
+    const testValue = "Jose";
+    const e = new Manager(testValue, "id", "email", "officeNumber");
+    expect(e.getName()).toBe(testValue);
+ });
+
+test('7 (G) should get id via getId()', () => { 
+    const testValue = 81;
+    const e = new Manager("name", testValue, "email", "officeNumber");
+    expect(e.getId()).toBe(testValue);
+ });
+
+test('8 (G) should get email via getEmail()', () => { 
+   const testValue = "josejpd3@gmail.com";
+   const e = new Manager("name", "id", testValue, "officeNumber");
+   expect(e.getEmail()).toBe(testValue);
+});
+
+test('9 (G) should get officeNumber via getOfficeNumber()', () => { 
+    const testValue = "0910";
+    const e = new Manager("name", "id", "email", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+ });
+
+test('10 (G) should get role via getRole()', () => { 
+    const testValue = "Manager";
+    const e = new Manager(testValue);
+    expect(e.getRole()).toBe(testValue);
+ });
