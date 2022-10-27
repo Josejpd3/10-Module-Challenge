ff1726dfdf37dacb02903f4744d47d72904aeba0
Jose Plasencia <josejpd3@gmail.com>
2022-10-27 18:23:59 -0400

    tested instantiating Engineer instance

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,6 @@
 const Engineer = require("../lib/Engineer")
+
+test('1 (I) should instantiate Engineer instance', () => { 
+    const e = new Engineer();
+    expect(typeof(e)).toBe("object");
+ });
