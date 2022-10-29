b60617e563cc094e51e97aefc09fd2e425704278
Jose Plasencia <josejpd3@gmail.com>
2022-10-28 21:24:16 -0400

    added start html template literals

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,23 @@
 function createHtml(teamMembers) {
+    const start = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/f1d7a5917a.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <link rel='stylesheet' href='./style.css'>
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header class="flex justify-center items-center bg-red-500 h-32">
+            <h1 class="text-4xl text-white font-medium">My Team</h1>
+        </header>
+    
+        <main>
+            <section class="card-section flex flex-row flex-wrap justify-center">
+`;
 
 };
 
