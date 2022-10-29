58a18c941fdc35746e9c2da9b3219fbd09879d95
Jose Plasencia <josejpd3@gmail.com>
2022-10-28 21:22:50 -0400

    createHTML function

--- src/page-template.js
+++ src/page-template.js
@@ -1,29 +1,5 @@
-const generateTeam = team => {
+function createHtml(teamMembers) {
 
-    // create the manager html
-    const generateManager = manager => {
-        return `
-        
-        `;
-    };
+};
 
-    // create the html for engineers
-    const generateEngineer = engineer => {
-        return `
 
-        `;
-    };
-
-    // create the html for interns
-    const generateIntern = intern => {
-        return `
-
-        `;
-    };
-
-    // use the team array to generate pieces of html based on the employee role
-}
-
-module.exports = team => {
-    // template literal - html body that calls the generate team function
-}
